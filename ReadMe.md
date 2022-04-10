68573332cdd19e397e0a1e9fabcff0163d5aef1b
polizoto <polizoto@hotmail.com>
2022-04-09 20:57:35 -0400

    initial files and back end code

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# openSource-bookFinder
+
+An MERN stack open-source bookFinder using Google Books API
+
+## Technologies Used
+
+- Apollo Server
+- GraphQL API
+- React
+- React hooks
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+input SavedBooks {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+  type User {
+    _id: ID
+    username: String!
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookInfo: SavedBooks): User
+    deleteBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
